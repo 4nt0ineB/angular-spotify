40734912e3dfdafc0f1ae55e4953f08174e48937
cydaw6 <56827368+cydaw6@users.noreply.github.com>
2023-04-14 18:58:30 +0200

    2 files modified

--- src/app/main/navbar/navbar.component.ts
+++ src/app/main/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Playlist } from '../../model/models';
+import { SessionService } from '../../session.service';
 
 @Component({
   selector: 'app-navbar',
@@ -12,12 +14,21 @@ export class NavbarComponent implements OnInit {
     { path: 'search', label: 'Search', icon: 'search' },
     { path: 'search', label: 'Create Playlist', icon: 'library_add' },
   ];
+  playlists: Playlist[];
 
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private session: SessionService
+  ) {}
 
   changeView(childroute: string) {
     this.router.navigate([childroute], { relativeTo: this.route });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.session.getPlaylists().subscribe((res: Playlist[]) => {
+      this.playlists = res;
+    });
+  }
 }
