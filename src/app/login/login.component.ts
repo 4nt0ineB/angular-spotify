e47788efd8a4592be41b0a33658cb13f87fe4e46
cydaw6 <56827368+cydaw6@users.noreply.github.com>
2023-04-14 11:51:00 +0200

    3 files modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,7 +7,19 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  username: string;
   constructor() {}
+
+  @Output()
+  usernameEmitter = new EventEmitter<string>();
+
+  submit(form: NgForm): void {
+    if (!form.valid) {
+      return;
+    }
+    this.usernameEmitter.emit(this.username);
+    form.reset();
+  }
 
   ngOnInit() {}
 }
