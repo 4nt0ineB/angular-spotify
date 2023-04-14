62126ee1aa35d3d73b48d0dcd8acb5f6024b11af
cydaw6 <56827368+cydaw6@users.noreply.github.com>
2023-04-14 21:44:41 +0200

    3 files modified

--- src/app/main/profile/profile.component.ts
+++ src/app/main/profile/profile.component.ts
@@ -11,6 +11,11 @@ export class ProfileComponent implements OnInit {
   playlists: Playlist[];
   constructor(public session: SessionService) {}
 
+  getRandomColor() {
+    var color = Math.floor(0x1000000 * Math.random()).toString(16);
+    return '#' + ('000000' + color).slice(-6);
+  }
+
   ngOnInit() {
     this.session.getPlaylists().subscribe((res: Playlist[]) => {
       this.playlists = res;
