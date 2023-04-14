94eb725cf21244f4140e4618cdfd926bc9f470d6
cydaw6 <56827368+cydaw6@users.noreply.github.com>
2023-04-14 12:47:37 +0200

    2 files modified, 2 added

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -3,10 +3,25 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from '../login/login.component';
 import { MainComponent } from '../main/main.component';
+import { UserComponent } from '../main/user/user.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'user/:username', component: MainComponent },
+  {
+    path: 'user/:username',
+    component: MainComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      },
+      {
+        path: 'profile',
+        component: UserComponent,
+      },
+    ],
+  },
 ];
 
 @NgModule({
