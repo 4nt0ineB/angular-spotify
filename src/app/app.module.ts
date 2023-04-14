138dd3f806f3585f28f7aadbcd5a848edae3c4be
cydaw6 <56827368+cydaw6@users.noreply.github.com>
2023-04-14 11:27:59 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, AppRoutingModule],
   declarations: [LoginComponent, AppComponent],
   bootstrap: [AppComponent],
 })
